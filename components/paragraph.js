5ab70df2b150b36dab3e53d5d7acb3ceadfa8c43
井手翔陽 <shouhi605@gmail.com>
2022-04-24 00:02:35 +0900

    paragraph component 修正

--- components/paragraph.js
+++ components/paragraph.js
@@ -3,6 +3,8 @@ import styled from '@emotion/styled'
 const Paragraph = styled.p`
   text-align: justify;
   text-indent: 1em;
+  margin: 2% auto;
+  font-size: smaller;
 `
 
 export default Paragraph
